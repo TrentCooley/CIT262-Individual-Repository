bdf56514428dadfe03b167f6da868225de5bd6fe
Trent Cooley <Cooley.trent@gmail.com>
2023-05-31 11:04:46 -0600

    completed the W07 assignments

---
+++ cit262-stedi-app/server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {res.send("Hello Browser")});
+
+app.get('/trent', (req,res) => {res.send("Hello Trent")});
+
+app.listen(port, ()=>console.log("Listening"));
